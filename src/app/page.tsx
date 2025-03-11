6ec2d429e15d2e39b2f85d87eaa656a83d38e97a
vinay bhat <150057416+bhatvinay7@users.noreply.github.com>
2025-03-11 09:58:58 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-white min-h-screen h-full place-content-center w-full flex itms-center justify-center my-auto ">
-      <h1 className="text-black text-2xl md:text-3xl text-center ">Welcome To AccessGate</h1>
+      <h1 className="text-black text-2xl md:text-3xl text-center mt-1/2 ">Welcome To AccessGate</h1>
     </div>
   );
 }
