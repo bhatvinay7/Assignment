e80ac00bd2291e14ea7e9cb956227553ebe475b8
vinay bhat <150057416+bhatvinay7@users.noreply.github.com>
2025-03-12 14:12:21 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ return NextResponse.redirect(new URL("/signIn", request.url));
 }
     
     const res= NextResponse.next();
-        res.headers.set("Access-Control-Allow-Origin", "https://assignment-ten-self-27.vercel.app"); // Change '*' to specific domains if needed
+        res.headers.set("Access-Control-Allow-Origin", "*"); // Change '*' to specific domains if needed
         res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
     return res;
